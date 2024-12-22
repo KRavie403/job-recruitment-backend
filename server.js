0b7f3659223a6e2ca4cee93278f0e285c369471a
Zoe <yoo9379@gmail.com>
2024-12-23 06:57:05 +0900

    updated: server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const crawlerRoutes = require("./routes/crawler");
+const jobsRoutes = require("./routes/jobs");
+const companiesRoutes = require("./routes/companies");
+const applicationsRoutes = require("./routes/applications");
+const stationsRoutes = require("./routes/stations");
+const bookmarksRoutes = require("./routes/bookmarks");
+const educationRoutes = require("./routes/education_requirements");
+const experienceRoutes = require("./routes/experience_requirements");
+const userRoutes = require("./routes/users");
+
 
 const app = express();
 const PORT = 8080;
@@ -9,7 +18,16 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get("/api/crawl", crawlerRoutes);
+// Routes
+app.use("/api/jobs", jobsRoutes);
+app.use("/api/companies", companiesRoutes);
+app.use("/api/applications", applicationsRoutes);
+app.use("/api/stations", stationsRoutes);
+app.use("/api/bookmarks", bookmarksRoutes);
+app.use("/api/education-requirements", educationRoutes);
+app.use("/api/experience-requirements", experienceRoutes);
+app.use("/api/users", userRoutes);
+// app.get("/api/crawl", crawlerRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
